5459c1a7c5265c6b960b8e4f5007f6a48d1f25a1
bustena <bustena@gmail.com>
2026-09-15 21:48:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -558,20 +558,3 @@ mutationObserver.observe(document.body, {
   attributes: true,
   characterData: true
 });
-
-window.addEventListener('resize', enviarAltura);
-
-const resizeObserver = new ResizeObserver(enviarAltura);
-
-Array.from(document.body.children).forEach(el => {
-  resizeObserver.observe(el);
-});
-
-const mutationObserver = new MutationObserver(enviarAltura);
-
-mutationObserver.observe(document.body, {
-  childList: true,
-  subtree: true,
-  attributes: true,
-  characterData: true
-});
